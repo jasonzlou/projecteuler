f9264a685f3c501d6990010d76d617ade3800cd0
jasonzlou <56004808+jasonzlou@users.noreply.github.com>
2020-10-31 22:45:29 -0400

    Create q10.cpp

---
+++ q10.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+//not good run time but better with space usage than creating a large boolean array
+bool isPrime(int n) {
+	
+	if (n == 2) {
+		return true;
+	} else if (n % 2 == 0) {
+		return false;
+	}
+	
+	for (int i = 3; i < n; i+=2) {
+		if (n % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main() {
+	
+	int below(2000000);
+	long sum(0);
+	
+	for (int i = 2; i < below; i++) {
+		if (isPrime(i)) {
+			sum += i;
+		}
+	}
+	
+	cout << sum;
+	
+	return 0;
+}
